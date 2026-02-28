767bde5cff57795e80e844538f8de2c65e0f7632
Vitalii <saifutdinov_vitalii@mail.ru>
2026-02-28 15:59:35 +0500

    hgf

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,18 +3,5 @@
             loop: true,
             slidesPerView: 1,
             spaceBetween: -50,
-            allowTouchMove: true,
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: -10,
-                },
-                450: {
-                    slidesPerView: 1,
-                    spaceBetween: -30,
-                },
-                1024: {
-                    slidesPerView: 1,
-                },
-        }
-    })
+            allowTouchMove: true,        
+        })
